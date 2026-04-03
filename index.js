798d986efeabcb6ed33ebf0231ed15843bd4219f
smileofuka-creator <smileofuka@gmail.com>
2026-04-03 12:37:32 +0800

    changed index js

---
+++ index.js
@@ -0,0 +1,389 @@
+// let number = 23;
+// let number2 = 14;
+
+// let niilber = number2 + 24;
+// let gar = number - number2;
+
+// console.log(niilber);
+// console.log();
+
+// let a = 10;
+// let b = 20;
+// let c = a + b;
+
+// console.log(c);
+// console.log(a + b);
+
+// let name = "Batbayar ";
+// let name1 = "Tsedensodnom";
+// let fullname = name + name1;
+
+// console.log(name + name1);
+// console.log(fullname);
+
+// let a = 1;
+// let b = 5;
+// let c = 3;
+// let niilber = a + b;
+// let niilber1 = a + b + c;
+// let urjver = a * b;
+
+// console.log(niilber);
+// console.log(niilber1);
+// console.log(urjver);
+
+// let a = 2;
+// let b = 5;
+// let c = -7;
+
+// let x1 = (-b + Math.sqrt(b + b - 4 * a * c)) / (2 * a);
+// let x2 = (-b - Math.sqrt(b + b - 4 * a * c)) / (2 * a);
+
+// console.log(x1, x2);
+
+// let a = 4;
+// let b = 2;
+
+// let p = 2 * (a + b);
+// let s = a * b;
+
+// console.log(p, s);
+
+// let firstname = "Batbayar ";
+// let lastname = "Tsedensodnom";
+// let gender = " whatever";
+// let age = 89;
+// let phonenumber = "88888882";
+// let email = " galzuugants@gmail.com";
+// // let info = firstname + lastname + gender + age + phonenumber + email;
+// // console.log(info);
+// console.log(firstname, lastname, gender, age, phonenumber, email);
+
+// let a = 5;
+// a = a * a + 5;
+
+// console.log(a);
+
+// let b = 5;
+// b = b / b - 5;
+// console.log(b);
+
+// let a = 4;
+// let b = 5;
+
+// let A = (a * b) / 2;
+
+// console.log(A);
+
+// let a = 4;
+// let b = 3;
+
+// let c = Math.sqrt(a * a + b * b);
+
+// console.log(c);
+
+// var year, yearJohn, yearMark;
+
+// year = 2025;
+// ageJohn = 28;
+// ageMark = 48;
+// console.log("Ene jil bol : " + year);
+
+// yearJohn = year - ageJohn;
+// yearMark = year - ageMark;
+// console.log("Jonii tursun jil " + yearJohn);
+// console.log("markin tursun jil  " + yearMark);
+
+// var future = year + 30;
+// console.log(future);
+
+// yearfutureMark = year + 18;
+// console.log("Mark ireeduid " + yearfutureMark);
+
+// var johnolder = ageJohn > ageMark;
+
+// console.log(johnolder);
+
+//  4 sarin 1 nii udur
+
+// let dun = 75;
+// let dun1 = "D avkaa";
+// let dun2 = "C avlaa";
+// let dun3 = "B avlaa";
+// let dun4 = "A avlaa";
+// let dun5 = "Shine yumaa suraa hu";
+
+// if (dun <= 59) {
+//   console.log("F avchee urshuulgui");
+// } else if (dun <= 69) {
+//   console.log("dun1");
+// } else if (dun <= 79) {
+//   console.log("dun2");
+// } else if (dun <= 89) {
+//   console.log("dun3");
+// } else if (dun <= 95) {
+//   console.log("dun4");
+// } else {
+//   console.log("dun5");
+// }
+
+// let pass = 24
+
+// if(pass === "admin123") { console.log("zuv");}
+// else {console.log ("pass buruu bn");}
+
+// let nas = 35
+
+// if(pass >=21) { console.log("orj bolno");}
+// else {console.log("Nasand hureegui");}
+
+// let age = 13;
+
+// if (age <= 18) {
+//   console.log("huuhed");
+// } else if (age <= 22) {
+//   console.log("oyutan");
+// } else if (age <= 65) {
+//   console.log("tom hun");
+// } else {
+//   console.log("undur nastan");
+// }
+
+// let a = 5;
+// let b = 8;
+
+// if (a > b) {
+//   console.log("a" + " ih");
+// } else if (a < b) {
+//   console.log("b" + " ih");
+// } else {
+//   console.log("false");
+// }
+
+// let temp = 15;
+
+// if (temp >= 30) {
+//   console.log("hot");
+// } else if (20 < temp > 30) {
+//   console.log("dulaan");
+// } else if (temp <= 20) {
+//   console.log("huiten");
+// }
+
+// let year = 2031;
+
+// if (year % 400 === 0) {
+//   console.log("undur jil");
+// } else {
+//   if (year % 100 === 0) {
+//     console.log("ene bol undur jil bishee");
+//   } else {
+//     if (year % 4 === 0) {
+//       console.log("undur");
+//     } else {
+//       console.log("undur bishee");
+//     }
+//   }
+// }
+
+// if (year % 4 === 0) {
+//   if (year % 100 === 0)
+//     if (year % 400 === 0) {
+//       console.log("undur");
+//     } else {
+//       console.log("undur jil bish");
+//     }
+
+// if (year % 4 === 0) {
+//     if(year % 100 === 0){
+//         console.log("Bish")
+//     }
+//     else {
+//         if(year % 400 === 0){
+//             console.log("undur jil")
+//         }
+//         else {
+//             console.log("undur jil")
+//         }
+//     }
+// }
+
+// let jil = 2028;
+
+// if (jil % 400 === 0) {
+//   console.log("undur jil");
+// } else if (jil % 100 === 0) {
+//   console.log("undur jil bish");
+// } else if (jil % 4 === 0) {
+//   console.log("undur jil");
+// } else {
+//   console.log("undur jil bish");
+// }
+
+// let operator = (+, -, /, * );
+// let a = 1;
+// let b = 5;
+
+// let
+// console.log(a - b);
+
+// if ()
+
+// calculator;
+
+// let operator = "-";
+// let a = 1;
+// let b = 4;
+
+// if (operator == "+") {
+//   console.log(a + b);
+// } else if (operator == "-") {
+//   console.log(a - b);
+// } else if (operator == "*") {
+//   console.log(a * b);
+// } else if (operator == "/") {
+//   console.log(a / b);
+// } else {
+//   console.log("buruu shuu");
+// }
+
+// let q = 5;
+// let w = "5";
+
+// if (q == w) {
+//   console.log("tentsuu");
+// } else {
+//   console.log("tentsuu bish");
+// }
+
+// let num = 18.9;
+
+// if (num % 2 === 0) {
+//   console.log("tegsh");
+// } else {
+//   console.log("sondgoi");
+// }
+
+// 4 sarin 2 n udur
+
+// let isWeather = false;
+// let haveFriends = false;
+
+// if (isWeather && haveFriends) {
+//   console.log("hicheelee hii");
+// } else if (!isWeather || haveFriends) {
+//   console.log("gertee toglono");
+// } else if (isWeather || !haveFriends) {
+//   console.log("gertee toglono");
+// } else {
+//   console.log("hul bumbug toglono");
+// }
+
+// if (!isWeather || haveFriends) {
+//   console.log("gertee toglono");
+// }
+
+// if (isWeather || !haveFriends) {
+//   console.log("gertee toglono");
+// }
+
+// 2 dahi bodlogo
+
+// let age = 16;
+// let haveTicket = false;
+// let parentAgreed = true;
+
+// if (age > 18 && haveTicket) {
+//   console.log("toglolt uzne");
+// // } else if (age > 18 || !haveTicket) {
+// //   console.log("toglolt uzehgui");
+// } else if (age < 18 && parentAgreed) {
+//   console.log("toglolt uzne");
+// } else {
+//   console.log("toglolt uzehgui");
+// }
+
+//3. User login
+//password username taarah, blocklogdoogui baival nevtruulne
+//password username taaraad blocktoi baival "blockoo garguulnuu"
+// password username 2iin ali negn taarahgui bol "Username or password wrong"
+
+// let username = "sar";
+// let password = "a";
+// let blocked = false;
+// // let hi = true;
+// // a = !hi;
+// // console.log(a);
+
+// if (username === "sar" && password === "a" && !blocked) {
+//   console.log("nevtruulne");
+// } else if (username === "sar" && password === "a" && blocked) {
+//   console.log("blockoo garguul");
+// } else {
+//   console.log("username or password wrong");
+// }
+
+// if (username === "sar" && password === "a" && !blocked)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     ) {
+//   console.log("nevtruulne");
+
+// }
+//  else if (blocked) {
+//   console.log("blockoo garguul");
+// }
+// else {
+//   console.log("username or password wrong");
+// }
+
+// let nevtreh =
+//   username === "sar" && password === "a" && !blocked
+//     ? "nevterle"
+//     : blocked
+//       ? "blockoo garga"
+//       : "username or pass wrong";
+// console.log(nevtreh);
+
+// 5 dahi udur
+
+// for (let i = 10; 0 <= i; i = i - 1) {
+//   console.log(i);
+// }
+
+// for (let i = 1; 10 > i; i = i + 3) {
+//   console.log(i);
+// }
+
+// for (let i = 1; i < 100; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i + "tegsh too");
+//   } else {
+//     console.log(i + "sondgoi too");
+//   }
+// }
+
+// let sum = 0;
+// for (let i = 1; i < 100; i++, sum = sum + i);
+// {
+//   console.log(sum);
+// }
+
+// let y = 1;
+// for (let i = 1; i < 100; i = i + 2, y = y * i) {
+// //   console.log(y);
+// // }
+
+// let star = "";
+// for (i = 1; i <= 10; i++) {
+//   star = star + "*";
+//   console.log(star);
+// }
+
+
+
+// for (let i = 1; i <= 10; i++)
+//   for (let j = 0; j <= 5; j++) {
+//     console.log(i, "*", j, "=", i * j);
+//   }
+
+let stars = "*"
+
+for( let i = 1; i<=5;  )
